5c7d5025a295223e77a2ac3dfc93bfa02749dc0d
igooogwork <148974994+igooogwork@users.noreply.github.com>
2023-10-25 23:31:02 +0300

    delete readme.txt and upgrade all catalog /python

--- python/2.py
+++ python/2.py
@@ -1,7 +1,11 @@
+# Написать функцию is_year_leap, принимающую 1 аргумент — год,
+# и возвращающую True, если год високосный, и False иначе.
 year = input('Введите год:')
 year = int(year)
 leap_year = year % 4
 if leap_year == 0:
-    print('Год високосный')
+    is_year_leap = True
+    print(is_year_leap)
 else:
-    print('Год невисокосный') 
+    is_year_leap = False
+    print(is_year_leap)
