e5296b7b49d8cbe13f757f640f9bbb9ff0edf7ca
igooogwork <148974994+igooogwork@users.noreply.github.com>
2023-10-27 13:36:30 +0300

    upgrade six.py

--- python/Python_Tasks/six.py
+++ python/Python_Tasks/six.py
@@ -3,28 +3,32 @@
 import sys
 
 def contains_digit(number):
-    return all(char.isdigit() for char in str(number))
+     return all(char.isdigit() for char in str(number))
     
 def is_prime(number):
-    if number < 2 or number > 1000:
-        return False
+    if number < 0 or number > 1000:
+        print("Программа завершена - введенное число не соответствует условию.")
+        sys.exit(1)
+    
     sieve = [True] * (1001) 
     sieve[0] = sieve[1] = False
+    
     for p in range(2, int(number**0.5) + 1):
         if sieve[p]:
             for i in range(p * p, 1001, p):
                 sieve[i] = False
+    
     return sieve[number]
-
+        
 number_to_check = input('Введите число:')
 
 if contains_digit(number_to_check):
     number_to_check = int(number_to_check)
 else:
-    print("Программа завершена - некорректный ввод")
+    print("Программа завершена - некорректный ввод или введено отрицательное число.")
     sys.exit(1)
     
 if is_prime(number_to_check):
-    print(f"{number_to_check} - простое число")
+    print(True)
 else:
-    print(f"{number_to_check} - не простое число")    
+    print(False)    
