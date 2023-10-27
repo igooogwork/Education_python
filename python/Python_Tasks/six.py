231742696dddaafff7c4e106e58ac32a56a77556
igooogwork <148974994+igooogwork@users.noreply.github.com>
2023-10-27 12:52:36 +0300

    upgrade six.py

--- python/Python_Tasks/six.py
+++ python/Python_Tasks/six.py
@@ -1,5 +1,10 @@
 # Написать функцию is_prime, принимающую 1 аргумент — число
 # т 0 до 1000, и возвращающую True, если оно простое, и False - иначе.
+import sys
+
+def contains_digit(number):
+    return all(char.isdigit() for char in str(number))
+    
 def is_prime(number):
     if number < 2 or number > 1000:
         return False
@@ -11,4 +16,15 @@ def is_prime(number):
                 sieve[i] = False
     return sieve[number]
 
-        
+number_to_check = input('Введите число:')
+
+if contains_digit(number_to_check):
+    number_to_check = int(number_to_check)
+else:
+    print("Программа завершена - некорректный ввод")
+    sys.exit(1)
+    
+if is_prime(number_to_check):
+    print(f"{number_to_check} - простое число")
+else:
+    print(f"{number_to_check} - не простое число")    
